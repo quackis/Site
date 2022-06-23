981f493a19eef4f5fde1be02310c74e672b207af
Jake Anderson <realjakeanderson@gmail.com>
2022-06-22 20:20:04 -0700

    commit

---
+++ Music/byband.js
@@ -0,0 +1,18 @@
+
+function num() {
+    var x = document.getElementById("num");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function a() {
+    var x = document.getElementById("a");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
