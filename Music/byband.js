dda58199ec9825da134ce13b0dc0580f37c04777
Jake Anderson <realjakeanderson@gmail.com>
2022-06-27 14:57:32 -0700

    commit

--- Music/byband.js
+++ Music/byband.js
@@ -16,3 +16,28 @@ function a() {
         x.style.display = "block";
     }
 }
+
+var divs = ["num", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var visibleDivId = null;
+    
+    function divVisibility(divId) {
+      if(visibleDivId === divId) {
+        visibleDivId = null;
+      } else {
+        visibleDivId = divId;
+      }
+      hideNonVisibleDivs();
+    }
+
+    function hideNonVisibleDivs() {
+      var i, divId, div;
+      for(i = 0; i < divs.length; i++) {
+        divId = divs[i];
+        div = document.getElementById(divId);
+        if(visibleDivId === divId) {
+          div.style.display = "block";
+        } else {
+          div.style.display = "none";
+        }
+      }
+    }
