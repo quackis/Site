4e0d70f5f6bc6f8cf3b6cc2df1d177c7525c0e2d
Jake Anderson <realjakeanderson@gmail.com>
2022-07-08 15:28:15 -0700

    commit

--- Music/bygenre.js
+++ Music/bygenre.js
@@ -1,6 +1,6 @@
 var divs = ["num", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var visibleDivId = null;
-
+    
     function divVisibility(divId) {
       if(visibleDivId === divId) {
         visibleDivId = null;
